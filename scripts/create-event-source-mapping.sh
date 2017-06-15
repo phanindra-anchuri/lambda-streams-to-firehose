f75a5117dbe1e4d226ff7c087775539b8b166a87
Phanindra Anchuri <phanindra.anchuri@climate.com>
2017-06-15 14:14:37 -0700

    script changes for creating event source mapping

--- scripts/create-event-source-mapping.sh
+++ scripts/create-event-source-mapping.sh
@@ -2,16 +2,17 @@
 
 DEFAULT_ENVIRONMENT=qa1
 ENVIRONMENT=${1:-$DEFAULT_ENVIRONMENT}
+TABLE_NAME=${2}
 LAMBDA_FUNCTION_NAME=platform-lambda-ddb-streams-to-firehose
 
 pip install --upgrade --user awscli
 
-streamarn=$(aws dynamodbstreams list-streams --table-name com.climate.platform.acquisition.uploads | jq '.Streams[0].StreamArn')
+streamarn=$(aws dynamodbstreams list-streams --table-name ${TABLE_NAME} | jq '.Streams[0].StreamArn' | tr -d '"')
 
-echo ${streamarn}
+echo "Stream ARN ${streamarn}"
 
 aws lambda create-event-source-mapping \
 --function-name ${LAMBDA_FUNCTION_NAME} \
 --starting-position LATEST \
---event-source-arn arn:aws:dynamodb:us-east-1:345062758380:table/com.climate.platform.acquisition.uploads/stream/2017-06-07T17:14:31.030 \
+--event-source-arn ${streamarn} \
 --enabled
